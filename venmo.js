35ad8593a184f129a1c6ff9c25333f8ac8255943
dhalima3 <dhalima@gatech.edu>
2015-12-15 23:37:11 -0500

    use process env variables

--- venmo.js
+++ venmo.js
@@ -1,14 +1,11 @@
 var page = require('webpage').create();
-var system = require('system');
-var username = "XXXXXX"
-var password = "XXXXXX"
 
 page.open('https://venmo.com/', function(status) {
     var status = page.evaluate(function() {
         if (document.title == "Venmo - Share Payments") {
             document.getElementsByClassName("sign-in").click();
-            document.querySelector("input[name='username']").value = username;
-            document.querySelector("input[name='password']").value = password;
+            document.querySelector("input[name='username']").value = process.env.username;
+            document.querySelector("input[name='password']").value = process.env.password;
             document.getElementsByClassName("login").click();
             console.log("logged in");
         }

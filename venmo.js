8735346d1d05d1dc758832e6d57256e51da0db9e
dhalima3 <dhalima@gatech.edu>
2015-12-16 21:59:57 -0500

    implement login

--- venmo.js
+++ venmo.js
@@ -1,17 +1,73 @@
+var steps = [];
+var testindex = 0;
+var loadInProgress = false;
 var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36';
+page.settings.javascriptEnabled = true;
+page.settings.loadImages = false;//Script is much faster with this field set to false
+phantom.cookiesEnabled = true;
+phantom.javascriptEnabled = true;
 
-page.open('https://venmo.com/', function(status) {
-    var status = page.evaluate(function() {
-        if (document.title == "Venmo - Share Payments") {
-            document.getElementsByClassName("sign-in").click();
+steps = [
+    function() {
+        console.log("Step 1 - Open Home Page");
+        page.open("https://venmo.com/", function(status) {
+
+        });
+    },
+
+    function() {
+        console.log("Step 2 - Click the Sign in Button");
+        page.evaluate(function() {
+            document.getElementsByClassName("sign-in")[0].click();
+        });
+    },
+
+    function() {
+        console.log("Step 3 - Fill in login form and submit it");
+        page.evaluate(function() {
             document.querySelector("input[name='username']").value = process.env.username;
             document.querySelector("input[name='password']").value = process.env.password;
-            document.getElementsByClassName("login").click();
-            console.log("logged in");
-        }
-    });
+            document.getElementsByClassName("login")[0].click();
+            console.log("Success Login");
+        });
+    },
 
-    window.setTimeout(function() {
+    function() {
+        console.log("Step 4 - Wait to login. Woo");
+        var fs = require('fs');
+        var result = page.evaluate(function() {
+            return document.querySelectorAll("html")[0].outerHTML;
+        });
+        fs.write('VenmoLoggedIn.html', result, 'w');
+    },
+];
+
+interval = setInterval(executeRequestsStepByStep, 5000);
+
+function executeRequestsStepByStep(){
+    if (loadInProgress == false && typeof steps[testindex] == "function") {
+        steps[testindex]();
+        testindex++;
+    }
+    if (typeof steps[testindex] != "function") {
+        console.log("test complete!");
         phantom.exit();
-    }, 5000);
-});
+    }
+}
+
+/**
+ * These listeners are very important in order to phantom work properly. Using these listeners, we control loadInProgress marker which controls, weather a page is fully loaded.
+ * Without this, we will get content of the page, even a page is not fully loaded.
+ */
+page.onLoadStarted = function() {
+    loadInProgress = true;
+    console.log('Loading started');
+};
+page.onLoadFinished = function() {
+    loadInProgress = false;
+    console.log('Loading finished');
+};
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
